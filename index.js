b4d4eb8648bb2d17b6f9651f6d9b5d7552c8fc5c
César <80521851+Cesar-ctrl@users.noreply.github.com>
2022-06-19 21:12:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ const server = app.listen(PORT, () => {
 
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://babyguard.vercel.app",
     credentials: true,
   },
 });
