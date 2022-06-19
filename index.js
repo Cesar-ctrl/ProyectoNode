a48a67d7f318b27ee27a00210986769754b12096
César <80521851+Cesar-ctrl@users.noreply.github.com>
2022-06-19 13:10:49 +0200

    Un push de Limpieza

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const app = express()
 const path = require('path');
 const socket = require('socket.io')
 const User = require('./models/User')
-const Note = require('./models/Note')
 const Imagen = require('./models/Imagen')
 
 const logger = require('./loggerMiddlewhare')
@@ -80,102 +79,6 @@ app.get('/', (request, response) => {
 })
 
 
-app.get('/', (request, response) =>{
-    Note.find({}).then(notes => {
-      response.json(notes)
-  })
-})
-
-app.get('/api/notes', async (request, response) =>{
-  const notes = await Note.find({}).populate('user', {
-    username: 1,
-    name: 1
-  })
-  response.json(notes)
-})
-
-app.get('/api/notes/:id', (request, response, next) =>{
-    const { id } = request.params
-    
-    Note.findById(id)
-    .then(note => {
-        if (note){
-            return response.json(note)
-        } else {
-            response.status(404).end()
-        }
-    }).catch(err => {
-        next(err)   
-    })
-    
-})
-
-app.delete('/api/notes/:id', userExtractor, async (request, response, next) => {
-    const { id } = request.params
-    // const note = await Note.findById(id)
-    // if (!note) return response.sendStatus(404)
-  
-    const res = await Note.findByIdAndDelete(id)
-    if (res === null) return response.sendStatus(404)
-  
-    response.status(204).end()
-})
-
-app.post('/api/notes', userExtractor, async (request, response, next) => {
-    const {
-      content,
-      important = false
-    } = request.body
-  
-    // sacar userId de request
-    const { userId } = request
-  
-    const user = await User.findById(userId)
-  
-    if (!content) {
-      return response.status(400).json({
-        error: 'required "content" field is missing'
-      })
-    }
-  
-    const newNote = new Note({
-      content,
-      date: new Date(),
-      important,
-      user: user._id
-    })
-  
-    // newNote.save().then(savedNote => {
-    //   response.json(savedNote)
-    // }).catch(err => next(err))
-  
-    try {
-      const savedNote = await newNote.save()
-  
-      user.notes = user.notes.concat(savedNote._id)
-      await user.save()
-  
-      response.json(savedNote)
-    } catch (error) {
-      next(error)
-    }
-})
-
-app.put('/api/notes/:id', userExtractor, (request, response, next) => {
-    const { id } = request.params
-    const note = request.body
-  
-    const newNoteInfo = {
-      content: note.content,
-      important: note.important
-    }
-  
-    Note.findByIdAndUpdate(id, newNoteInfo, { new: true })
-      .then(result => {
-        response.json(result)
-      })
-      .catch(next)
-  })
 //-------------------------MI APP------------------------------------
 app.use('/api/img/public', express.static(`${__dirname}/uploads`))
 
