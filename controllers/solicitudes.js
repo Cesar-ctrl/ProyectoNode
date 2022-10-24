700491640263f9988941f02302bbae7678d4409b
César <80521851+Cesar-ctrl@users.noreply.github.com>
2022-10-24 09:26:54 +0200

    Post y Put añadidos
    Añadidos Post y Put de Solicitudes, no están probados

--- controllers/solicitudes.js
+++ controllers/solicitudes.js
@@ -3,33 +3,55 @@ const Solicitud = require('../models/User')
 const userExtractor = require('../middleware/userExtractor')
 
 solicitudesRouter.get('/:id', async (request, response) => {
-    try {
-        //from y to son o un usuario y un guard No se pueden comunicar entre usuarios ni entre guards
-        const { id } = request.params
-        const { user, guard } = request.body;
-        
-        //Busca todos los mensajes que tengan el usuario especificado
-        await Solicitud.find({
-            guard:id
-        }).populate('user', {
-            id:1,
-            name:1,
-            surnames:1,
-            imgUrl:1
-        })
-        .then(user => {
-            if (user){
-                return response.json(user)
-            } else {
-                response.status(404).end()
-            }
-        }).catch(err => {
-            console.log(err)   
-        })
 
-      } catch (ex) {
-        next(ex);
-      }
+    //from y to son o un usuario y un guard No se pueden comunicar entre usuarios ni entre guards
+    const { id } = request.params
+    const { user, guard } = request.body;
+    
+    //Busca todos los mensajes que tengan el usuario especificado
+    await Solicitud.find({
+        guard:id
+    }).populate('user', {
+        id:1,
+        name:1,
+        surnames:1,
+        imgUrl:1
+    })
+    .then(user => {
+        if (user){
+            return response.json(user)
+        } else {
+            response.status(404).end()
+        }
+    }).catch(err => {
+        console.log(err)   
+    })
+
+
+})
+
+solicitudesRouter.post('/', async (request, response) => {
+
+    const { user, guard } = request.body;
+    const solicitud = new Solicitud({
+        user,
+        guard
+    })
+    const savedSolicitud = await solicitud.save()
+    response.status(201).json(savedSolicitud)
+})
+
+solicitudesRouter.put('/:id', async (request, response) => {
+
+    const { id } = request.params
+    const { aprobado } = request.body;
+    const respuesta = {aprobado:aprobado}
+    
+    await Solicitud.findByIdAndUpdate(id, respuesta, { new: true })
+    .then(result => {
+        response.json(result)
+      })
+      .catch(next)
 })
 
 module.exports = solicitudesRouter
